e7156b4ee2fdac5d5c4fcbc6f24552a8da111400
Micael Santos <micael.pereira1999@hotmail.com>
2022-01-28 00:09:40 -0300

    feat: adding list ip allowed to cors

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const cors = (req: Request, res: Response, next: NextFunction): void => {
-  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-origin', process.env.CORS_IP_ALLOWED || '*')
   res.set('access-control-allow-headers', '*')
   res.set('access-control-allow-methods', '*')
   next()
